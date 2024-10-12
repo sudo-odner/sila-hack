4e90bf36035ee2a03d63a01098bea2d9663bb423
macbook <playegrovoy@gmail.com>
2024-10-12 19:06:09 +0300

    pdf file fixed

--- data/neuro/LaptopNeuroDataSource.py
+++ data/neuro/LaptopNeuroDataSource.py
@@ -6,7 +6,7 @@ class LaptopNeuroDataSource:
         filePath = f"image_src/{id}.jpg"
         with open(filePath, 'rb') as file:
             files = {'file': (filePath, file, 'image/jpg')}
-            response = requests.post("http://213.173.108.217:17256/ml/upload-image/", files=files)
+            response = requests.post("http://213.173.108.217:10989/ml/upload-image/", files=files)
             callback(
                 id,
                 response.json()["data"],
